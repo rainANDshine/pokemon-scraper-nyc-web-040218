6832b47e10f2564d54f49ee17ea32813b72b328f
Shun Yao <ysmiracle@hotmail.com>
2018-04-11 01:59:03 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,11 @@
 class Pokemon
+  def initialize(name, type, db)
+    @name = name
+    @type = type
+    @db = db
+  end
+
+  def self.save(name, breed, age, database_connection)
+    database_connection.execute("INSERT INTO cats (name, breed, age) VALUES (?, ?, ?)",name, breed, age)
+  end
 end
