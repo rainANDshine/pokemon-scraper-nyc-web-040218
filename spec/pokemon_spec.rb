b59eb4887ce409f25bb0c2594dd1e1f22674d3b7
Shun Yao <ysmiracle@hotmail.com>
2018-04-11 02:46:17 +0000

    automatically backed up by learn

--- spec/pokemon_spec.rb
+++ spec/pokemon_spec.rb
@@ -1,5 +1,5 @@
 require_relative "spec_helper"
-
+require 'pry'
 describe "Pokemon" do
   before do
     @db = SQLite3::Database.new(':memory:')
@@ -33,6 +33,7 @@ describe "Pokemon" do
       # The find method creates a new Pokemon after selecting their row from the database by their id number.
       Pokemon.save("Pikachu", "electric", @db)
       pikachu_from_db = Pokemon.find(1, @db)
+      binding.pry
       expect(pikachu_from_db.id).to eq(1)
       expect(pikachu_from_db.name).to eq("Pikachu")
       expect(pikachu_from_db.type).to eq("electric")
