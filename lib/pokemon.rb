20991f2a08f1d0d079052987f3d8e2a888236145
Shun Yao <ysmiracle@hotmail.com>
2018-04-11 02:25:04 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,11 +1,11 @@
 class Pokemon
-  attr_accessor :id, :name, :type, :age, :db
+  attr_accessor :id, :name, :type, :db
 
   def initialize(attributes)
     attributes.each {|key, value| self.send(("#{key}="), value)}
   end
 
-  def self.save(name, breed, age, database_connection)
-    database_connection.execute("INSERT INTO cats (name, breed, age) VALUES (?, ?, ?)",name, breed, age)
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type, db) VALUES (?, ?, ?)",name, type, db)
   end
 end
